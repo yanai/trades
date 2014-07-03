9a87f0c2c315ca3d6542ddc70603a5fb9c6d138e
Yanai Franchi <yanai@tikalk.com>
2014-07-03 09:09:22 +0300

    Move to Log4J

--- src/main/java/com/com/tikal/traders/service/impl/TradeService.java
+++ src/main/java/com/com/tikal/traders/service/impl/TradeService.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.com.tikal.traders.domain.entity.Trader;
 import com.com.tikal.traders.domain.entity.Transaction;
