807d94f83e90613237eb36edb504d141ed7babd5
yanai <yanai@tikalk.com>
2015-11-17 21:48:08 +0200

    spring

--- src/main/java/com/com/tikal/traders/Application.java
+++ src/main/java/com/com/tikal/traders/Application.java
@@ -2,17 +2,16 @@ package com.com.tikal.traders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 	
 	
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 
 //		ConfigurableApplicationContext context = SpringApplication.run(Application.class);
 		SpringApplication.run(Application.class, args);
