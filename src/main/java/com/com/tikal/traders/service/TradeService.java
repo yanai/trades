0a90a00d5fdb68c6b283ac89af95a958863755c9
yanai <yanai@tikalk.com>
2015-12-29 11:54:17 +0200

    Tx

--- src/main/java/com/com/tikal/traders/service/TradeService.java
+++ src/main/java/com/com/tikal/traders/service/TradeService.java
@@ -7,6 +7,7 @@ import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ import com.com.tikal.traders.repository.jpa.TransactionRepository;
 import com.com.tikal.traders.repository.memory.TransactionMemoryRepository;
 
 @RestController
+@Transactional
 public class TradeService {
 	
 	
